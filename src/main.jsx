235a4172a8cf19bab960f7f15d3f37033700eac8
React1 <purvesh0207@gmail.com>
2026-09-22 12:30:09 +0530

    fix: configure router base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import "./index.css";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/hr-stack-frontend">
         <App />
     </BrowserRouter>
 );
-
-// we are going to create multiple pages wrapped in browserrouter
